8876e59840b3c429f42c976e2901bd5ed644de34
DanielleM <dlmedeiro@gmail.com>
2022-06-15 18:58:20 -0700

    Exercise complete - need to verify

---
+++ src/helpers.js
@@ -0,0 +1,21 @@
+// src/helpers.js
+// This file should export two array helper functions:
+
+// choice(items): returns a randomly selected item from array of items
+// remove(items, item): removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
+
+const choice = (items) => {
+    const idx = Math.floor(Math.random() * items.length);
+    return (items[idx])
+}
+
+const remove = (items, item) => {
+    for (let i = 0; i < items.length; i++){
+        if (items[i] === item) {
+            newList = items.slice(0,i) + items.slice(i+1)
+            return newList
+        }
+    }
+}
+
+export {choice, remove}
